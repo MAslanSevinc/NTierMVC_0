e44ea557702377e8e147f532eaf3d3c5523b885c
Aslan <Aslan@DESKTOP-FA89NTH>
2022-02-14 02:38:32 +0300

    Add project files.

---
+++ Project.BLL/DesignPatterns/GenericRepository/IntRep/IRepository.cs
@@ -0,0 +1,51 @@
+﻿using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.DesignPatterns.GenericRepository.IntRep
+{
+    public interface IRepository<T> where T:BaseEntity
+    {
+        //ListCommands
+
+        List<T> GetAll();
+        List<T> GetActives();
+        List<T> GetPassives();
+        List<T> GetModifieds();
+
+
+        //ModifyCommands
+        void Add(T item);
+        void AddRange(List<T> list);
+        void Delete(T item);
+        void DeleteRange(List<T> list);
+        void Update(T item);
+        void UpdateRange(List<T> list);
+        void Destroy(T item);
+        void DestroyRange(List<T> list);
+
+
+        //Linq Commands
+
+        List<T> Where(Expression<Func<T, bool>> exp);
+        bool Any(Expression<Func<T, bool>> exp);
+        T FirstOrDefault(Expression<Func<T, bool>> exp);
+        object Select(Expression<Func<T, object>> exp);
+
+
+
+
+        //FindCommands
+        T Find(int id);
+
+        //Custom Commends
+ 
+        T GetLastData();
+        T GetFirstData();
+
+    }
+}
